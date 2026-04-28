e83f0bac4deab9338cdceae78049c75ca61a7e78
Amit Gupta <amitgupta4263@gmail.com>
2026-04-29 00:12:24 +0530

    Add index.html and update traffic light logic in one.js

--- JSpractice/Logics/one.js
+++ JSpractice/Logics/one.js
@@ -1,7 +1,7 @@
-//  how works traffic light 
-let color = "Green";
+//  how works traffic light Using if Statement
+let color = "Blue";
 
-if(color === "red"){
+if(color === "Red"){
     console.log("Stop Here");
 }
 if(color === "yellow"){
@@ -9,4 +9,42 @@ if(color === "yellow"){
 }
 if(color === "Green"){
     console.log(" Don't stop! Go");
+}else(color === "Blue");
+    console.log("Traffic Rule is Brocken");
+
+
+// using else if statement
+
+let age = 19;
+
+if(age >= 18){
+    console.log("You are Adult go for voting");
+}else if(age >= 16 ){
+    console.log("U can't Able for Votion");
+}
+else if(age >= 22){
+    console.log("Go For voting Because u are Super Adult");
+}
+
+
+//  Rank Student system
+let Rank = 69;
+if(Rank >= 88){
+    console.log("A+");
+}else if(Rank >= 66){
+    console.log("B")
+}else if(Rank == 75){
+    console.log("A")
+}
+
+// winter cheaker system
+
+let month = "April";
+
+if(month === " jaunary"){
+    console.log("Winter is here");
+}else if(month === "April"){
+    console.log("Summer is here");
+}else if(month == "September"){
+    console.log("Raining is here");
 }
