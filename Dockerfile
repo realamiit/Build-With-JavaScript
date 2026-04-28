c0c65654cca2974a472026a7dae5368feba3f958
Amit Gupta <amitgupta4263@gmail.com>
2026-04-28 22:58:14 +0530

    Remove unused files: Dockerfile, README.md, index.html, script.js, and style.css

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu:latest
-LABEL authors="reala"
-
-ENTRYPOINT ["top", "-b"]
---
+++ JSpractice/Logics/one.js
@@ -0,0 +1,11 @@
+let color = "red";
+
+if(color === "red"){
+    console.log("Stop Here");
+}
+if(color === "yellow"){
+    console.log("Ready to go or Slow Go");
+}
+if(color === "Green"){
+    console.log(" Don't stop! Go");
+}
