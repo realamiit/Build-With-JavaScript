5014b18b29c9127a4b7b45b69bbcd433d6af60fa
Amit Gupta <amitgupta4263@gmail.com>
2026-04-29 16:30:47 +0530

    Add SwitchStatement.js and truthyAndFalcy.js to demonstrate switch-case logic and truthy/falsy values in JavaScript

---
+++ JSpractice/Logics/SwitchStatement.js
@@ -0,0 +1,17 @@
+let color = "red";
+
+switch (color){
+    case "red" :
+        console.log("stop")
+        break;
+    case "yellow" :
+        console.log("Slow Doun")
+        break;
+    case "green" :
+        console.log("Go")
+        break;
+    default : 
+        console.log("BrRak the light")
+}
+
+console.log("After yje syam");
