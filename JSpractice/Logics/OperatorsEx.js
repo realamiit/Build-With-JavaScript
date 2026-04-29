07998fd7bc42320b8b2d581b5bd66aa4e590772f
Amit Gupta <amitgupta4263@gmail.com>
2026-04-29 15:05:42 +0530

    Add OperatorsEx.js to implement string evaluation logic

---
+++ JSpractice/Logics/OperatorsEx.js
@@ -0,0 +1,6 @@
+// Find A Good String given the string
+let str = "a p p l e";
+if((str[0] === "a") && (str.length > 3)){
+    console.log("Good String");
+}else
+    console.log("The String is Not a good String");
