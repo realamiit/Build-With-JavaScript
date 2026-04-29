991303ecff5ae27dd74282629e2c622413010086
Amit Gupta <amitgupta4263@gmail.com>
2026-04-29 15:16:30 +0530

    Refactor OperatorsEx.js to improve string evaluation logic and add safety checks

--- JSpractice/Logics/OperatorsEx.js
+++ JSpractice/Logics/OperatorsEx.js
@@ -4,3 +4,13 @@ if((str[0] === "a") && (str.length > 3)){
     console.log("Good String");
 }else
     console.log("The String is Not a good String");
+
+
+    // Expect the  output of this code
+
+    let num = 15;
+
+    if((num%3 === 0) && ((num+1 === 16) || (num-1 === 11))){
+        console.log("Safe");
+    }else
+        console.log("Not Safe");
