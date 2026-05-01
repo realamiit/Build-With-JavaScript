920a42890a09f668673416868aa0d37a3218eded
Amit Gupta <amitgupta4263@gmail.com>
2026-05-01 18:00:58 +0530

    Add Replace.js for string replacement and repetition examples; update index.html to reference Replace.js

---
+++ JSpractice/StringMethod/Replace.js
@@ -0,0 +1,13 @@
+let str = "I love coding in every midnight";
+
+str = str.replace("love", "Do");
+console.log(str);
+
+
+//  Repeat method
+
+
+let Str = "Hare krishna jai sheree radha ";
+
+Msg = Str.repeat(108);
+console.log(Msg);
