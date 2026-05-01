2a8ed50c94113836ed12c5783cc2bf90b43d1449
Amit Gupta <amitgupta4263@gmail.com>
2026-05-01 17:05:01 +0530

    Add MethodWithArgu.js for string manipulation; update index.html to reference MethodWithArgu.js

--- JSpractice/StringMethod/Upper&Lower.js
+++ JSpractice/StringMethod/Upper&Lower.js
@@ -5,3 +5,5 @@ let petAmimals = " Cat dog Cow Buffelow";
 let myclassmate = "  amit ayush saurabh prashant devesh yogesh Ankur "
 console.log(petAmimals.toLowerCase());
 console.log(myclassmate.toUpperCase());
+
+
