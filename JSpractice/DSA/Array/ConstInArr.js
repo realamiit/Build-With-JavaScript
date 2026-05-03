b90af23e9e01488ff92e118d12b384d59bd9f1be
Amit Gupta <amitgupta4263@gmail.com>
2026-05-03 18:42:53 +0530

    Add ConstInArr.js and NestedArray.js to demonstrate array manipulation and nested array structure

---
+++ JSpractice/DSA/Array/ConstInArr.js
@@ -0,0 +1,16 @@
+const num = [2,3,45,6,7];
+
+num.push(9);
+num.pop()
+num.pop()
+
+console.log(num);
+
+newnum = num;
+console.log(newnum);
+
+
+const arr = ["A","B","C","D","E"];
+
+num = arr; //Here u got error" Assignment to constant variable." mtlb by  the assress the are constant that's because (num = arr) not possible 
+console.log(arr);
