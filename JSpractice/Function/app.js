7613404dc530d11ce8f7bbca99dd437d4f4370a7
Amit Gupta <amitgupta4263@gmail.com>
2026-05-25 20:31:19 +0530

    Add app.js to demonstrate basic console logging and error handling

---
+++ JSpractice/Function/app.js
@@ -0,0 +1,17 @@
+console.log("one");
+console.log("two");
+console.log("three");
+
+
+setTimeout(() => {
+    console.log("hello Chunmun meyauuu");
+
+},5000); // 4 sec
+try {
+  return kjzdnbkbsdhb;
+} catch (err) {
+  console.log(err.message); // "kjzdnbkbsdhb is not defined"
+}
+
+console.log("five");
+console.log("9");
