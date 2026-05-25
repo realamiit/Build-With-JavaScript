9cbb4f074b3662ecd4da8066bf42e1d421f13ce5
Amit Gupta <amitgupta4263@gmail.com>
2026-05-25 21:33:52 +0530

    Fix console log message and adjust setTimeout duration in app.js

--- JSpractice/Function/app.js
+++ JSpractice/Function/app.js
@@ -4,9 +4,9 @@ console.log("three");
 
 
 setTimeout(() => {
-    console.log("hello Chunmun meyauuu");
+    console.log("Hello world");
 
-},5000); // 4 sec
+},6000); // 4 sec
 
 try {
   return kjzdnbkbsdhb;
@@ -15,8 +15,13 @@ try {
 }
 
 console.log("five");
-console.log("9");
+console.log("Six");
 
 
 // Catch :let me accept the task
 // try :if task is false then , here is my backup plan 
+
+//  when we use this in the this flow like 0ne, two, three , four, [SetTimeoute] , five , six : it this flow than there 
+//  it this middle the function these  setTimeout  in we can go with set time out if time 0 sec (0 milisecond) than also go with the output is this flow
+//  in : {one ,two, three, four, five ,six, hello world}  this secquence follow for output does't metter the timing is zero milisecond or mwny mili second (5000)
+ 
