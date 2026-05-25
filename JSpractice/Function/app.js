94956b6ea7f01b0387f68af77a292d3063d4b3eb
Amit Gupta <amitgupta4263@gmail.com>
2026-05-25 21:37:24 +0530

    Remove unnecessary newline in app.js and clarify comments for better understanding

--- JSpractice/Function/app.js
+++ JSpractice/Function/app.js
@@ -2,7 +2,6 @@ console.log("one");
 console.log("two");
 console.log("three");
 
-
 setTimeout(() => {
     console.log("Hello world");
 
@@ -24,4 +23,4 @@ console.log("Six");
 //  when we use this in the this flow like 0ne, two, three , four, [SetTimeoute] , five , six : it this flow than there 
 //  it this middle the function these  setTimeout  in we can go with set time out if time 0 sec (0 milisecond) than also go with the output is this flow
 //  in : {one ,two, three, four, five ,six, hello world}  this secquence follow for output does't metter the timing is zero milisecond or mwny mili second (5000)
- 
+// it means after set timeout is it has to go with many process 
