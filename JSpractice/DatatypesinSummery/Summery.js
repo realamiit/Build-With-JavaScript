22f868da960f171a87009ca944e420cf3e3fda68
Amit Gupta <amitgupta4263@gmail.com>
2026-05-27 00:36:28 +0530

    Datatypes in summery

---
+++ JSpractice/DatatypesinSummery/Summery.js
@@ -0,0 +1,46 @@
+// premitive
+// 7 types: String, Number, Bollean , Null, Undefiend, Symbol, BigInt
+
+// Reference (Non Premitive)
+ // Array, Object, Functions
+
+ const score = 2000;
+
+const ScoreValue = 544.34;
+const rollNum = 2122239756;
+const isLoggedIN = false;
+const outsideTemp = null;
+
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+
+const BigNumber = 1223325344355444n;
+
+console.log(BigNumber);
+
+
+
+//Array creating
+const Heros= ["Shaktiman", "Nagaraj", "spiderman"]
+// creating Object
+let myObj = {
+    name: "AMit",
+    class: "BCA",
+    age: 22,
+}
+
+// function
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+// find Datatypes any value
+
+console.log(typeof  BigNumber);
