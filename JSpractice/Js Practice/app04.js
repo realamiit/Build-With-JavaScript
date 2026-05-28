a43e40851e647193526582177b3dc11673bc57ee
Amit Gupta <amitgupta4263@gmail.com>
2026-05-28 18:28:27 +0530

    claud task

---
+++ JSpractice/Js Practice/app04.js
@@ -0,0 +1,8 @@
+// const greet = "Hello , Amit! Welcome to javascript";
+
+function greet(name){
+    console.log("Hello "+name +"! welcome javascript")
+}
+greet("Ashish");
+greet("Aman");
+greet("Amit");
