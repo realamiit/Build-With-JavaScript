40984fc3305e5cdb45084c486091c163fbaf5b35
Amit Gupta <amitgupta4263@gmail.com>
2026-06-01 17:59:53 +0530

    summerise the code

--- JSpractice/Js Practice/app02.js
+++ JSpractice/Js Practice/app02.js
@@ -5,3 +5,11 @@ if(marks > 40){
 }else{
     console.log("mai Fail ho gya!")
 }
+
+
+// let marks in intiger 45 
+// yadi makrs > (greater than) 40 
+// if condition is true 
+// console (mao pass ho gya )
+// if condation is false the condation 
+// console" mai fail ho gya "
