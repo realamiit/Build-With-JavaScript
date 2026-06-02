0a4a65243e31d1cc656fdae302cddfc4bcf58851
Amit Gupta <amitgupta4263@gmail.com>
2026-06-02 17:38:44 +0530

    Creat object person and print it

---
+++ app.08.js
@@ -0,0 +1,18 @@
+let person = {
+    name: "Amit Gupta",
+    age: 22,
+    rollno : 11,
+    cource: "BCA",
+    college: "Dr Ghanshyam Singh College of Aducation Varanasi mohav",
+    city: "Varanasi",
+
+}
+
+console.log(person);
+
+
+// pehele eek person name se object bnaya jisme fir apni deatils store ki jai name 
+// "Amit Gupta"  in string , age 22 int , rollno 11 in int , Cource "BCA" , college "DGSCA" city "Varanasi",
+// also string  than closse the curl bracket  than the console .log ke udr for print krege person /
+//  than we get our all deatil in stored in the valu in person object i means extract  all the 
+// store value also  
