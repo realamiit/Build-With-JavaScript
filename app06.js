a5ed795d6ef8d092a8b475b69a85f6dce599d03d
Amit Gupta <amitgupta4263@gmail.com>
2026-06-02 17:43:49 +0530

    Creat object person and print it

---
+++ app06.js
@@ -0,0 +1,6 @@
+let fruits = ["Pinapple","Apple","Orange","Banana","mango"];
+
+console.log(fruits[0]);
+console.log(fruits[2]);
+console.log("we have total: "+fruits.length+" fruits");
+console.log(fruits[3]);
