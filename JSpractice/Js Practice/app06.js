a1663b4a9884a5e4e8f8f07ff7365978cb9a9dc0
Amit Gupta <amitgupta4263@gmail.com>
2026-06-05 01:20:23 +0530

    Resolved merge conflict

--- JSpractice/Js Practice/app06.js
+++ JSpractice/Js Practice/app06.js
@@ -6,3 +6,8 @@ console.log("we have total: "+fruits.length+" fruits");
 console.log(fruits[3]);
 
 //  lets summerize it 
+// pehele let fruits name ka eek variable bnaya usme store kiya some name of fruits and than 
+// i accress the all the fruits by the indexing of arr and inn  arr ki indexig 0 se start
+//  hoti hai to pehele element ko call krne ke liye fruits[0] kiya to array ki peheli element jo hai vo mil 
+// gyii isi trh [2] fir console me we have total fruits count krwane ke liye fruits.length use kiya ise hme sare  fruits na number 
+// bil gya kitne frruits hai hmdare fruits naame ke array me  tha again we print fruits[3] extractt  banana aaya jo ki arr ke 3rd pe hai
