8b794d3336736589e5ce2068a13f2ecbde35f7eb
Amit Gupta <amitgupta4263@gmail.com>
2026-06-05 01:21:28 +0530

    Resolved merge conflict

--- JSpractice/Js Practice/app03.js
+++ JSpractice/Js Practice/app03.js
@@ -1,16 +1,14 @@
-
-
-for(let i = 1; i<=5; i++){
-    console.log(i);
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
 }
 
-// in this pirticular for loop 
+// in this pirticular for loop
 // intiger i se start hoga aur condation cheak krega ki
-// i is greaterthan are equalto 5 than print i = 1 
-//  than again i++ krega fir i ki value 2 ho jayegi than again cheak the condation 
-// 2 is greater than arre equal to 5 condation true than o-print 2 , fir i++ 3 ho jayega fir condation cheak 
-// hoga i yani 3 is greater than are equal to 5 thana
-//  the print 3 thana again i++ 4 hoga than tha ahiain cheak condation than the i=4 is greaterthan 
-// are equalm to 5 tha condation iis truure than return thw i++ than 
+// i is greaterthan are equalto 5 than print i = 1
+//  than again i++ krega fir i ki value 2 ho jayegi than again cheak the condation
+// 2 is greater than arre equal to 5 condation true than o-print 2 , fir i++ 3 ho jayega fir condation cheak
+// hoga i yani 3 is greater than are equal to 5 than
+//  the print 3 thana again i++ 4 hoga than tha ahiain cheak condation than the i=4 is greaterthan
+// are equalm to 5 tha condation iis truure than return thw i++ than
 // the condation is true than return 5 than it always print in next line keep remember  after 5
-// i++ than 6 not true vthe condation is false it break the loop and stop it 
+// i++ than 6 not true vthe condation is false it break the loop and stop it
