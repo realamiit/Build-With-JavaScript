72750c6653821504327271be2be3cbfd25ac8b95
Amit Gupta <amitgupta4263@gmail.com>
2026-06-07 16:21:06 +0530

    Enhance app06.js with additional console logs
    Added console logs for fruits length and uppercase conversion.

--- JSpractice/Js Practice/app06.js
+++ JSpractice/Js Practice/app06.js
@@ -4,10 +4,14 @@ console.log(fruits[0]);
 console.log(fruits[2]);
 console.log("we have total: "+fruits.length+" fruits");
 console.log(fruits[3]);
+console.log(fruits.length);
+console.log(fruits.toUppercase);
 
-//  lets summerize it 
+//  lets summerize  
 // pehele let fruits name ka eek variable bnaya usme store kiya some name of fruits and than 
 // i accress the all the fruits by the indexing of arr and inn  arr ki indexig 0 se start
 //  hoti hai to pehele element ko call krne ke liye fruits[0] kiya to array ki peheli element jo hai vo mil 
 // gyii isi trh [2] fir console me we have total fruits count krwane ke liye fruits.length use kiya ise hme sare  fruits na number 
 // bil gya kitne frruits hai hmdare fruits naame ke array me  tha again we print fruits[3] extractt  banana aaya jo ki arr ke 3rd pe hai
+//  fruits kelength ko pta krooo
+// to use fruits.toUppercase  sb uppercase me ho jayga 
