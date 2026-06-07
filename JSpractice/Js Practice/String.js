e5520cfa73780fcc0e2adf1bf785cca6fbbee1a0
Amit Gupta <amitgupta4263@gmail.com>
2026-06-07 16:06:24 +0530

    Correct toUpperCase usage and update comments
    Fix typo in toUpperCase method and clean up comments.

--- JSpractice/Js Practice/String.js
+++ JSpractice/Js Practice/String.js
@@ -4,3 +4,5 @@ const repoCount = 50
 // console.log(name + repoCount + "Value");
 
 console.log('Hello my name is ${name.toUpprrcase} and my repo count is ${repoCount}');
+// it is agood practice 
+// to console .log in we print many more value thrue the name 
