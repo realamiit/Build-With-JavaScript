b11e758a256f76380eb7aa5f331a4b36fe2e62f8
Amit Gupta <amitgupta4263@gmail.com>
2026-06-11 22:27:29 +0530

    Ternary operator in one line because its tooo neet and clean look

---
+++ JSpractice/Js Practice/app13.js
@@ -0,0 +1,10 @@
+let marks = 75;
+
+if (marks>=40){
+    console.log("Pass");
+}else{
+    console.log("Fail");
+}
+
+   
+console.log(marks >= 40 ? "pass" : "Faail");   //in one line but same work and this is good practiice 
