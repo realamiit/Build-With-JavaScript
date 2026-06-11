c42e7badd0ccb12fe8883eaac67f9edd33c8cd28
Amit Gupta <amitgupta4263@gmail.com>
2026-06-11 23:21:45 +0530

    we use i++ externaly because the code is realiable and readable  summerize  for better understanding

--- JSpractice/Js Practice/app15.js
+++ JSpractice/Js Practice/app15.js
@@ -1,5 +1,9 @@
 let i = 1;
 while (i<=10){
-    console.log(i);
-    i++;
+    console.log(i);   
+    i++;        
 }
+
+// code ko realible and readable hiva bjaye eek hi 
+// line me console.log(i++) me ye likh skte but wwe are profectional broo 
+//  an the output is : 1 2 3 4 5 6 7 8 9 10
