43078d1d2427eaa0cee9aa12c14a243fb96b7650
Amit Gupta <amitgupta4263@gmail.com>
2026-06-20 00:00:06 +0530

    update and modify

--- JSpractice/Js Practice/JSpractice/DSA/Array/ArrayInRefence.js
+++ JSpractice/Js Practice/JSpractice/DSA/Array/ArrayInRefence.js
@@ -2,7 +2,7 @@ let arr = [1,2,3,4,5,6];
 
 let copyarr =arr;
 arr.push(9);
-arr.push("Amit");  // pass arr in string
+arr.push("Amit");  // pass arr in stringgg
 console.log(copyarr);
 copyarr.pop();
 console.log(arr);
