a58e2e8bdbc07450d200b80719b6d4182d799ec9
Amit Gupta <amitgupta4263@gmail.com>
2026-06-21 16:34:27 +0530

    Delet the extra created by default files

--- app01.js
+++
@@ -1,6 +0,0 @@
-let price =  100;
-let quantity = 5;
-
-console.log("Price hai : " + price);
-console.log("Quantity hai : "+ quantity);
-console.log("Total bill hai : "+ price*quantity);
--- app07.js
+++
@@ -1,5 +0,0 @@
-let fruits = ["Mango","Papaya","Banana","apple","MuskMelone","Litchi","Pinapple"];
-
-for(let i = 0; i< fruits.length;i++){
-    console.log(fruits[i]);
-};
