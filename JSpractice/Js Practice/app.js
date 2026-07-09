8b04e77bab3111a14e5b0cd45fd9538a2f7cceec
Amit Gupta <amitgupta4263@gmail.com>
2026-07-09 23:10:10 +0530

    Update app.js

--- JSpractice/Js Practice/app.js
+++ JSpractice/Js Practice/app.js
@@ -1,13 +1,15 @@
 const name = "Amit Gupta";
 let age = 22;
+let rollNo = 3;
+const subject = "MCA";
 
 console.log("Mera name : "+ name);
 console.log("My Age is : "+ age);
-console.log(" mai eek developer banuga");
+console.log("Mera age hai "+age+"  mai " + subject+ " kr rha hun ");
 
 
 // pehele constant name liya fir 
 // age liya integer 
 // than console.log me apna variable use Kiya 
 // than again console.log me age print krwayaa
-// the output is : "mai eek devellloper banuga
+// the output is : 
