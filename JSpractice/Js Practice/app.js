26b06684298a2839043ad11c604fd9767ca282fc
Amit Gupta <amitgupta4263@gmail.com>
2026-07-09 23:34:45 +0530

    Update app.js
    practice typeOf and create object and call and More

--- JSpractice/Js Practice/app.js
+++ JSpractice/Js Practice/app.js
@@ -3,13 +3,22 @@ let age = 22;
 let rollNo = 3;
 const subject = "MCA";
 
+const detailOfAmit {
+name = "Amit Gupta";
+age = 22;
+rollNo = 3;
+subject = "MCA";
+} 
+ let s === detailOfAmit;
 console.log("Mera name : "+ name);
 console.log("My Age is : "+ age);
 console.log("Mera age hai "+age+"  mai " + subject+ " kr rha hun ");
-
-
-// pehele constant name liya fir 
-// age liya integer 
-// than console.log me apna variable use Kiya 
-// than again console.log me age print krwayaa
-// the output is : 
+console.log(typeOf age);
+console.log(typeOf rollNo);
+console.log(detailOfAmit);
+console.log(typeOf detailOfAmit);
+ 
+s.name;
+s.age;
+s.rollNo;
+s.subject;
