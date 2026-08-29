dc9c106c95aa3c9812220e3c96885dddf3368f1b
Amit Gupta <amitgupta4263@gmail.com>
2026-08-29 23:34:39 +0530

    Modified Some code

--- JSpractice/StringMethod/Upper&Lower.js
+++ JSpractice/StringMethod/Upper&Lower.js
@@ -2,7 +2,11 @@ let str  = "Amit Gupta is here";
 
 let petAmimals = " Cat dog Cow Buffelow";
 
-let myclassmate = "  amit ayush saurabh prashant devesh yogesh Ankur "
+let myclassmate = "  amit ayush saurabh prashant devesh yogesh Ankur ";
+
+let students = "Harsh thugesh su=iddarth monkety Akash rahul pankaj sumit akshay"
+
+console.log(students.toLowerCase());
 console.log(petAmimals.toLowerCase());
 console.log(myclassmate.toUpperCase());
 
